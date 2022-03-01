5630218d972b71b8e8961715260afaf113d5c7b2
Carrie Stienen <carrie.stienen@gmail.com>
2022-03-01 14:49:08 -0600

    Added web server

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask, redirect, url_for, request, jsonify
+from project import main
+app = Flask(__name__)
+
+@app.route('/classify',methods = ['POST'])
+def classify():
+    content = request.json
+    dimensions = content['dimensions']
+    points = content['points']
+
+    result = main(dimensions,points)
+    return jsonify({"result":result})
+
+
+if __name__ == '__main__':
+   app.run(debug = True, host='0.0.0.0')
